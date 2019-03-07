120b81eb8914f772f1efe94b6745abdd1922a2eb
Alfonso <alfonsopidal@gmail.com>
2019-03-07 10:34:45 +0100

    test

--- src/ios/SegmentPlugin.m
+++ src/ios/SegmentPlugin.m
@@ -9,6 +9,7 @@
 {
     NSLog(@"[cordova-plugin-segment-sdk] plugin initialized");
 
+    // Fixed plugin
     NSLog(@"Advertising ID: ", [[[ASIdentifierManager sharedManager] advertisingIdentifier] UUIDString]);
 
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(finishLaunching:) name:UIApplicationDidFinishLaunchingNotification object:nil];
